495ab35ae1e35e8e7b09e5ee691f83e1b27146a1
Shawn Goulet <shawn.goulet@gmail.com>
2017-05-03 16:35:21 -0400

    adding packag.json (& node_modules/) and server.js - idleTimeoutMillis issue needs to be resolved now.  if problematic, might not be right approach.

--- server.js
+++ server.js
@@ -3,32 +3,66 @@ var app = express();
 
 app.get('/', function (req, res) {
 
-    var sql = require("mssql");
+  var sql = require("mssql");
 
-    // config for your database
-    var config = {
-        user: 'sa',
-        password: 'mypassword',
-        server: 'localhost',
-        database: 'SchoolDB'
-    };
+// var config = {
+//   user: 'sa',
+//   password: 'mypassword',
+//   server: 'localhost',
+//   database: 'SchoolDB'
+// };
+// config for the 'wMVP3_CapeCodMA' database
+var config = {
+  // domain: 'CCCOM',
+  user: 'DBAccess',
+  password: 'Acce$$DB',
+  // server: '192.138.212.28', //ACESS FROM EXTERNAL TO NETWORK? WHAT TRIVEDI WAS USING?
+  server: '10.10.1.174',
+  port: '65335',
+  database: 'wMVP3_CapeCodMA',
+  pool: {
+    max: 100,
+    min: 0,
+    idleTimeoutMillis: 30000
+  }
+};
 
-    // connect to your database
-    sql.connect(config, function (err) {
+// var config = {
+//   domain: 'BC'
+//   user: 'DBAccess',
+//   password: 'Acce$$DB',
+//   // server: '192.138.212.28', //ACESS FROM EXTERNAL TO NETWORK? WHAT TRIVEDI WAS USING?
+//   server: 'localhost',
+//   port: '8118',
+//   database: 'wMVP3_CapeCodMA'
+//   pool: {
+//     max: 100,
+//     min: 0,
+//     idleTimeoutMillis: 30000
+//   }
+// };
 
-        if (err) console.log(err);
+  // connect to your database
+  sql.connect(config, function (err) {
 
-        // create Request object
-        var request = new sql.Request();
+    if (err) console.log(err);
 
-        // query to the database and get the records
-        request.query('select * from Student', function (err, recordset) {
+    // create Request object
+    // var request = new sql.Request();
+    var wMVP3_CapeCodMARequest = new sql.Request();
 
-            if (err) console.log(err)
+    // query to the database and get the records
+    wMVP3_CapeCodMARequest.query('select top 25 * from WastewaterSource3', function (err, recordset) {
 
-            // send records as a response
-            res.send(recordset);
+      if (err) console.log(err)
 
-        });
+      // send records as a response
+      res.send(recordset);
+
     });
+  });
 });
+
+var server = app.listen(5000, function () {
+  console.log('Server up...');
+})
