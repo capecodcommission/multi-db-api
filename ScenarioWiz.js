cdcde015157cbc0e7bedff804827955cbcb7cfa5
Mario Carloni <mario.carloni@cccom.barnstablecounty.org>
2017-06-19 11:30:55 -0400

    added additional wmvp3 table calls, inluding parameterized call

--- ScenarioWiz.js
+++ ScenarioWiz.js
@@ -9,6 +9,7 @@ app.use(bodyParser.json());
 
 // tell the app to use the CORS Middleware
 app.use(function (req, res, next) {
+
   // Enable the CORS
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
@@ -18,12 +19,14 @@ app.use(function (req, res, next) {
 
 // set the server & port config as vars
 var server = app.listen(process.env.PORT || 8081, function () {
+
   var port = server.address().port;
+
   console.log('App now running on port', port);
 });
 
-// set the configuration necessary to connect to the 'wMVP3_CapeCodMA' database
-var wMVP3_CapeCodMAConfig = {
+// wMVP3_CapeCodMA' DB config object
+var wmvp3_DBConfig = {
   user: 'DBAccess',
   password: 'Acce$$DB',
   // server: '192.138.212.28', //ACESS FROM EXTERNAL TO NETWORK? WHAT TRIVEDI WAS USING?
@@ -39,13 +42,14 @@ var wMVP3_CapeCodMAConfig = {
 };
 
 // Estbalish a ScenarioWizQuery f(x) to connect to 'wMVP3_CapeCodMA' & get a response
-var  ScenarioWizQuery = function (res, query) {
+var  executeQuery = function (res, query, config) {
 
   // use mssql node package to connect to the 'wMVP3_CapeCodMA' db
-  sql.connect(wMVP3_CapeCodMAConfig, function (err) {
+  sql.connect(config, function (err) {
 
     // if there's an error connecting to the db, console.log it out
     if (err) {
+
       console.log("Error while connecting database :- " + err);
       res.send(err);
     }
@@ -55,59 +59,92 @@ var  ScenarioWizQuery = function (res, query) {
 
       // indicate connection to the 'wMVP3_CapeCodMA' database
       console.log('we are connected');
+
       // create Request object
       var request = new sql.Request();
 
       // query the database
       request.query(query, function (err, recordset) {
 
-        // **TODO** : create the stream for the request
-        // request.pipe(stream)
-        // request.stream = true;
-        //
-        // request.on('recordset', columns => {
-        // })
-        //
-        // request.on('row', row => {
-        // })
-        //
-        // request.on('error', err => {
-        // })
-        //
-        // request.on('done', result => {
-        // })
-
         // if there's an error in the query, console.log it out
         if (err) {
+
           console.log('Error while querying database :- ' + err);
           res.send(err);
         }
 
         // if there's no error, send the response
         else {
-          // pipe.on('error', err => {
-            // SOMETHING
-          // })
 
-          // pipe.on('finish', () => {
-            // SOMETHING SOMETHING
-          // })
           res.send(recordset);
         }
-
-        // request.on('done', result => {
-        //   console.log(result.rowsAffected)
-        // })
-
-      });
+      })
     }
-  });
-};
+  })
+}
 
-//GET API
+//GET ScenarioWiz table from wmvp3 DB
 app.get('/api/ScenarioWiz', function(req , res) {
+
   var query = 'select * from CapeCodMA.Scenario_Wiz';
-  ScenarioWizQuery (res, query);
+
+  executeQuery (res, query, wmvp3_DBConfig);
+});
+
+//GET TreatmentWiz table from wmvp3 DB
+app.get('/api/TreatmentWiz', function(req , res) {
+
+  var query = 'select * from CapeCodMA.Treatment_Wiz';
+
+  executeQuery (res, query, wmvp3_DBConfig);
+});
+
+//GET Embayments table from wmvp3 DB
+app.get('/api/Embayments', function(req , res) {
+
+  var query = 'select * from CapeCodMA.Embayments';
+
+  executeQuery (res, query, wmvp3_DBConfig);
+});
+
+//GET FTCoeff table from wmvp3 DB
+app.get('/api/FTCoeff', function(req , res) {
+
+  var query = 'select * from CapeCodMA.FTCoeff';
+
+  executeQuery (res, query, wmvp3_DBConfig);
+});
+
+// GET MATowns from wmvp3
+app.get('/api/MATowns', function(req , res) {
+
+  var query = 'select * from CapeCodMA.MATowns';
+
+  executeQuery (res, query, wmvp3_DBConfig);
+});
+
+// GET SubEmbayments from wmvp3
+app.get('/api/SubEmbayments', function(req , res) {
+
+  var query = 'select * from CapeCodMA.SubEmbayments';
+
+  executeQuery (res, query, wmvp3_DBConfig);
+});
+
+// GET Subwatersheds from wmvp3
+app.get('/api/Subwatersheds', function(req , res) {
+
+  var query = 'select * from CapeCodMA.Subwatersheds';
+
+  executeQuery (res, query, wmvp3_DBConfig);
+});
+
+// GET parcelMaster from wmvp3
+app.get('/api/parcelMaster/:id', function(req , res) {
+
+  var query = 'select * from CapeCodMA.parcelMaster WHERE scenario_id = ' + req.params.id;
+
+  executeQuery (res, query, wmvp3_DBConfig);
 });
 
 //POST API
